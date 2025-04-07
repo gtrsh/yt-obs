22ac90881bc36faf155d99a4f03def14652f05cb
Gregory Tereshko <gtrsh@users.noreply.github.com>
2025-04-07 23:35:27 +0300

    add async lib

--- yt-obs-server/project.clj
+++ yt-obs-server/project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/gtrsh/yt-observer"
   :license {:name "EULA"}
   :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/core.async "1.7.701"]
                  [cheshire "5.13.0"]]
   :main ^:skip-aot yt-obs-server.core
   :target-path "target/%s"
