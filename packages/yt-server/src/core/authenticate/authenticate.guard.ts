49c9f0914f1157b2a1371cf480ab1333d3814a37
Gregory Tereshko <gtrsh@users.noreply.github.com>
2025-12-07 16:51:24 +0300

    authentication: add decorator for public endpoints

--- packages/yt-server/src/core/authenticate/authenticate.guard.ts
+++ packages/yt-server/src/core/authenticate/authenticate.guard.ts
@@ -1,17 +1,29 @@
 import { FastifyRequest } from 'fastify'
 
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
 import { JwtService } from '@nestjs/jwt'
 
+import { IS_PUBLIC_KEY } from './authenticate.decorator.js'
 import { jwt } from './constants.js'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
+    private reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ])
+
+    if (isPublic) {
+      return true
+    }
+
     const request = context.switchToHttp().getRequest()
     const token = this.extractTokenFromHeader(request)
 
