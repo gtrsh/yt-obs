89186ab323efed7413ccc20534205a7e69991532
Gregory Tereshko <gtrsh@users.noreply.github.com>
2026-01-31 23:36:49 +0300

    test script for worker task

---
+++ packages/yt-server/scripts/test-channel.ts
@@ -0,0 +1,21 @@
+import { Queue } from 'bullmq'
+
+const connection = {
+  host: process.env.YTOBS_BULL_HOST,
+  port: Number(process.env.YTOBS_BULL_PORT),
+}
+
+const queue = new Queue('QUEUE_CHANNEL', { connection })
+
+const url = process.argv.at(2)
+
+if (!url) {
+  console.error('Usage: tsx add-channel.ts <url>')
+  process.exit(1)
+}
+
+await queue.add('channel', { url })
+
+console.log(`Added job with url: ${url}`)
+
+await queue.close()
