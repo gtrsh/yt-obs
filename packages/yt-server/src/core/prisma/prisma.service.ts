8ef2a31c9cfa127c7bfe3643b2c5e5e85404739a
Gregory Tereshko <gtrsh@users.noreply.github.com>
2026-02-06 04:51:38 +0300

    make fields omited from response

--- packages/yt-server/src/core/prisma/prisma.service.ts
+++ packages/yt-server/src/core/prisma/prisma.service.ts
@@ -1,13 +1,25 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common'
 import { PrismaClient, PrismaPg } from '@yt-obs/store-sql'
 
+const DELETED_AT_FIELD = { deletedAt: true }
+const OMMIT_MODELS = {
+  user: { ...DELETED_AT_FIELD },
+  channel: { ...DELETED_AT_FIELD },
+  channelInfo: { ...DELETED_AT_FIELD },
+  channelData: { ...DELETED_AT_FIELD },
+  channelTask: { ...DELETED_AT_FIELD },
+}
+
 @Injectable()
 class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.YTOBS_STORE_PG as string,
     });
-    super({ adapter });
+    super({
+      adapter,
+      omit: OMMIT_MODELS,
+    });
   }
 
   async onModuleInit() {
