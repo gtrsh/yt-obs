da036a36b1641787b255d3d66febe9a2d195df93
Gregory Tereshko <gtrsh@users.noreply.github.com>
2026-02-07 12:49:23 +0300

    update test queries

--- packages/yt-server/scripts/test-queries.ts
+++ packages/yt-server/scripts/test-queries.ts
@@ -17,4 +17,32 @@ const client = new PrismaClient({ adapter, log: ['query'] })
 // })
 // console.log(channels)
 
+// const userId = process.argv.at(2)
+// const channels = await client.userChannel.findMany({
+//   where: { userId },
+//   include: { channel: true }
+// })
+// console.log(channels)
+// console.log(channels.map(({ channel }) => channel))
+
+// const userId = process.argv.at(2)
+// const channelId = process.argv.at(3)
+//
+// const data = await client.channelInfo.findFirst({
+//   select: { info: true },
+//   where: {
+//     channel: {
+//       id: channelId,
+//       users: {
+//         some: {
+//           userId
+//         }
+//       }
+//     }
+//   }
+// })
+// console.log(data)
+// const info = data?.info ?? null
+// console.log(info)
+
 await client.$disconnect()
