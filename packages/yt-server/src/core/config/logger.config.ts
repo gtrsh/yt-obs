88b2951cd0f154c4e6c9aa7f7dca03a7a17faef2
Gregory Tereshko <gtrsh@users.noreply.github.com>
2026-04-27 15:42:46 +0300

    fix logger

--- packages/yt-server/src/core/config/logger.config.ts
+++ packages/yt-server/src/core/config/logger.config.ts
@@ -1,4 +1,7 @@
+import { RequestMethod } from '@nestjs/common'
+
 export const loggerConfig = {
+  forRoutes: [{ method: RequestMethod.ALL, path: '*splat' }],
   pinoHttp: {
     redact: {
       paths: ['req.headers.authorization'],
